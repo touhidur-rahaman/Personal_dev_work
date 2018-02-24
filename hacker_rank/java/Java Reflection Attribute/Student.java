623127100be7e8326467b6009b93889dd326ad6d
Touhidur Rahaman <touhidur.rahaman.antu@gmail.com>
2018-02-24 14:40:37 +0600

    java reflection attributes

---
+++ hacker_rank/java/Java Reflection Attribute/Student.java
@@ -0,0 +1,17 @@
+class Student{
+    private String name;
+    private String id;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void anothermethod(){  }
+  
+}
